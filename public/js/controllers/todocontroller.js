91f2d7316a08d5dfcfd5fc750ebea26c2e801762
Chris Jarvis <clj@ollure.com>
2015-06-05 10:52:41 -0500

    repaired todocontroller

--- public/js/controllers/todocontroller.js
+++ public/js/controllers/todocontroller.js
@@ -1,17 +1,17 @@
 controllers.controller('TodoCtrl', function ($scope, $http, $location) {
   $scope.message = 'Todo';
-  $scope.todo = {};
+  $scope.Todo = {};
 
-  $scope.todo = function(){
+  $scope.Todo = function(){
     console.log('you clicked the create a form button')
     $http({
           method: 'POST',
           url: '/api/tasks',
-          data: $scope.todo
+          data: $scope.Todo
         }).
         success(function (data, status, headers, config) {
           console.log('you added a task!', data);
-          $scope.todo = data.task;
+          $scope.Todo = data.task;
           $location.path('/todolist');
         }).
         error(function (data, status, headers, config) {
