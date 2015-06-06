02efaafe9bcb01f4b4f98a05b4e4cd7af31b3f0a
Chris Jarvis <clj@ollure.com>
2015-06-05 23:32:25 -0500

    tasks saving to db

--- public/js/controllers/todocontroller.js
+++ public/js/controllers/todocontroller.js
@@ -1,43 +1,43 @@
 controllers.controller('TodoCtrl', function ($scope, $http, $rootScope, $location) {
   $scope.message = 'Todo';
-  $scope.todo = {};
+  $scope.task = {};
 
   $scope.todo = function(){
     console.log('you clicked the create a form button')
+    $scope.task.user = $rootScope.rootUser;
     $http({
-          method: 'GET',
-          url: '/api/tasks',
-          data: $scope.todo
-        }).
-        success(function (data, status, headers, config) {
-          console.log(' GET you added a task!', data);
-          console.log($scope.todo);
-          $scope.todo={};
-          $rootScope.rootuser = data.username;
-          $scope.todo = data.tasks;
-          console.log($scope.todo);
-
-          $location.path('/');
-        }).
-        error(function (data, status, headers, config) {
-          console.log(' GET adding task failed')
-        });
-
-        $http({
-          method: 'POST',
-          url: '/api/tasks',
-          data: $scope.user
-        }).
-        success(function (data, status, headers, config) {
-          console.log('Here is your list', data);
-          $scope.todo = data.tasks;
-          $location.path('/todolist');
-        }).
-        error(function (data, status, headers, config) {
-          console.log('adding task failed')
-        });
+      method: 'POST',
+      url: '/api/tasks',
+      data: $scope.task
+    }).
+    success(function (data, status, headers, config) {
+      console.log('Here is your list', $scope.task);
+      $scope.task = {};
+      $location.path('/todolist');
+    }).
+    error(function (data, status, headers, config) {
+      console.log('adding task failed')
+    });
 
   }
 
 });
 
+    // $http({
+    //       method: 'GET',
+    //       url: '/api/tasks',
+    //       data: $scope.todo
+    //     }).
+    //     success(function (data, status, headers, config) {
+    //       console.log(' GET you added a task!', data);
+    //       console.log($scope.todo);
+    //       $scope.todo={};
+    //       $rootScope.rootuser = data.username;
+    //       $scope.todo = data.tasks;
+    //       console.log($scope.todo);
+
+    //       $location.path('/todolist');
+    //     }).
+    //     error(function (data, status, headers, config) {
+    //       console.log(' GET adding task failed')
+    //     });
--- public/js/controllers/todolistcontroller.js
+++ public/js/controllers/todolistcontroller.js
@@ -1,23 +1,44 @@
-controllers.controller('TodolistCtrl', function ($scope, $http, $location) {
-    $scope.message= 'TodoList';
+controllers.controller('TodolistCtrl', function ($scope, $http, $rootScope, $location) {
+    $scope.message= 'TodoList page';
     $scope.todolist= {};
+    $scope.task = {};
 
 
   $scope.todolist = function(){
-    console.log('Heres your list')
+    console.log('TodolistCtrl is here')
     $http({
-          method: 'POST',
-          url: '/api/tasks',
-          data: $scope.user
-        }).
-        success(function (data, status, headers, config) {
-          console.log('Congratulations you have more stuff to do!', data);
-          $scope.todolist = data.tasks;
-          $location.path('/todolist');
-        }).
-        error(function (data, status, headers, config) {
-          console.log("nothing to do? check again you ain't getting off that esay")
-        });
+        method: 'GET',
+        url: '/api/tasks',
+        data: $scope.task
+      }).
+      success(function (data, status, headers, config) {
+        console.log('Congratulations you have more stuff to do!', $scope.task);
+        $scope.todolist = data.tasks;
+        $location.path('/todolist');
+      }).
+      error(function (data, status, headers, config) {
+        console.log("nothing to do? check again you ain't getting off that easy")
+      });
   }
 
 });
+
+// $http({
+//           method: 'GET',
+//           url: '/api/tasks',
+//           data: $scope.todo
+//         }).
+//         success(function (data, status, headers, config) {
+//           console.log(' GET you added a task!', data);
+//           console.log($scope.todo);
+//           $scope.todo={};
+//           $rootScope.rootuser = data.username;
+//           $scope.todo = data.tasks;
+//           console.log($scope.todo);
+
+//           $location.path('/todolist');
+//         }).
+//         error(function (data, status, headers, config) {
+//           console.log(' GET adding task failed')
+//         });
+
